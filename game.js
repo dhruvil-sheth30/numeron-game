d42afdcbdbe03d4cb026751dc69db8041ec328d8
Dhruvil Sheth <dhruvilsheth@proton.me>
2023-11-02 18:00:51 +0530

    First

--- game.js
+++ game.js
@@ -1,5 +1,82 @@
-// Iteration 2: Generate 2 random number and display it on the screen
+var number1 = Math.round(Math.random() * 100);
+const number1box = document.getElementById('number1');
+number1box.innerText = number1;
 
-// Iteration 3: Make the options button functional
+var number2 = Math.round(Math.random() * 100);
+const number2box = document.getElementById('number2');
+number2box.innerText = number2;
 
-// Iteration 4: Build a timer for the game
+const greaterThan = document.getElementById('greater-than');
+const equalTo = document.getElementById('equal-to');
+const lesserThan = document.getElementById('lesser-than');
+
+var score = 0;
+
+greaterThan.onclick = function() {
+    if(number1 > number2) {
+        score++;
+        resetTime(timerId);
+    } else {
+        location.href = './gameover.html';
+    }
+
+    number1 = Math.round(Math.random() * 100);
+    number1box.innerText = number1;
+
+    number2 = Math.round(Math.random() * 100);
+    number2box.innerText = number2;
+}
+
+lesserThan.onclick = function() {
+    if(number2 > number1) {
+        score++;
+        resetTime(timerId);
+    } else {
+        location.href = './gameover.html';
+    }
+
+    number1 = Math.round(Math.random() * 100);
+    number1box.innerText = number1;
+
+    number2 = Math.round(Math.random() * 100);
+    number2box.innerText = number2;
+}
+
+equalTo.onclick = function() {
+    if(number2 === number1) {
+        score++;
+        resetTime(timerId);
+    } else {
+        location.href = './gameover.html';
+    }
+
+    number1 = Math.round(Math.random() * 100);
+    number1box.innerText = number1;
+
+    number2 = Math.round(Math.random() * 100);
+    number2box.innerText = number2;
+}
+
+var time = 5;
+var timer = document.getElementById('timer');
+var timerId;
+
+function startTimer() {
+    time = 5;
+    timer.innerText = time;
+    timerId = setInterval(() => {
+        time--;
+        if (time === 0) {
+            location.href = "./gameover.html";
+        }
+        timer.innerText = time;
+        localStorage.setItem("score", score);
+    }, 1000);
+}
+
+function resetTime(intervalId) {
+    clearInterval(intervalId);
+    startTimer();
+}
+
+startTimer();
